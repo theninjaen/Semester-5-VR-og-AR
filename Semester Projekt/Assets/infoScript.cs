1b271dc5c2e52228b19c825f1abcad0207a47bc7
SaethorMH <smhspilari@gmail.com>
2023-11-28 22:46:05 +0100

    AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH modeller

---
+++ Semester Projekt/Assets/infoScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class infoScript : MonoBehaviour
+{
+    public int counter =0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
